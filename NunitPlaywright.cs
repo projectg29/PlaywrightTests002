25aeb9ac4820cf6f8c353b2a8131c88d6463fa85
whoami projectg29 <whoami.g4dad@gmail.com>
2022-07-20 07:12:32 +1200

    POM pattern started

--- NunitPlaywright.cs
+++ NunitPlaywright.cs
@@ -6,24 +6,30 @@ namespace PlaywrightTests001;
 public class NunitPlaywright : PageTest
 {
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        //When you want to run in cmdline mode and with browser open set below env option and run test
+        //PS C:\Users\rajan\Automation\DotNet\PlaywrightDemos\PlaywrightTests002 > $env: HEADED = 1
+        //PS C:\Users\rajan\Automation\DotNet\PlaywrightDemos\PlaywrightTests002 > dotnet test
+        
+        // The default timeout period is 3000 milliseconds
+        await Page.GotoAsync("http://www.eaapp.somee.com");
+
     }
 
     [Test]
     public async Task Test1()
     {
-        await Page.GotoAsync("http://www.eaapp.somee.com");
-        await Page.ClickAsync("text=Login");
-        await Page.ScreenshotAsync(new PageScreenshotOptions
-        {
-            Path = "PTest001.jpg"
-        });
 
+        var lnkLogin = Page.Locator("text=Login");
+        await lnkLogin.ClickAsync();
+        //await Page.ClickAsync("text=Login");
+        await Page.ScreenshotAsync(new PageScreenshotOptions { Path = "PTest001.jpg" });
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
-        await Page.ClickAsync("text=Log in");
-      
+        var btnLogin = Page.Locator("input", new PageLocatorOptions { HasTextString = "Log in" });
+        await btnLogin.ClickAsync();
+        //await Page.ClickAsync("text=Log in");
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
 
 
---
+++ Pages/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests002.Pages
+{
+    public class LoginPage
+    {
+        private IPage _page;
+        private readonly ILocator _lnkLogin;
+        private readonly ILocator _txtUserName;
+        private readonly ILocator _txtPassword;
+        private readonly ILocator _btnLogin;
+        private readonly ILocator _lnkEmployeeDetails;
+        
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+            _btnLogin = _page.Locator("text=Login");
+            _txtUserName = _page.Locator("#UserName");
+            _txtPassword = _page.Locator("#Password");
+            _btnLogin = _page.Locator("text=Log in");
+            _lnkEmployeeDetails = _page.Locator("text='Employee Details'");
+
+
+        }
+
+        public async Task ClickLogin() => await _lnkLogin.CheckAsync(); // This is call expression body syntax. It reduces the line of code in C#
+
+
+    }
+}
